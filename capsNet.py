983c6aba5b972a8d17c890c8267ff19f8f06f6b6
wjh720 <wjh720.eric@gmail.com>
2018-01-16 01:13:18 +0800

    123

--- capsNet.py
+++ capsNet.py
@@ -22,16 +22,14 @@ class CapsNet(object):
                 self.X, self.labels = get_batch_data(cfg.dataset, cfg.batch_size, cfg.num_threads)
                 self.Y = tf.one_hot(self.labels, depth=10, axis=1, dtype=tf.float32)
 
-                with tf.variable_scope('pi'):
-                    with tf.variable_scope('actor'):
-                        self.build_arch()
-                        self.loss()
-                        self._summary()
+                self.build_arch()
+                self.loss()
+                self._summary()
 
-                        # t_vars = tf.trainable_variables()
-                        self.global_step = tf.Variable(0, name='global_step', trainable=False)
-                        self.optimizer = tf.train.AdamOptimizer()
-                        self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)  # var_list=t_vars)
+                # t_vars = tf.trainable_variables()
+                self.global_step = tf.Variable(0, name='global_step', trainable=False)
+                self.optimizer = tf.train.AdamOptimizer()
+                self.train_op = self.optimizer.minimize(self.total_loss, global_step=self.global_step)  # var_list=t_vars)
             else:
                 self.X = tf.placeholder(tf.float32, shape=(cfg.batch_size, 28, 28, 1))
                 self.labels = tf.placeholder(tf.int32, shape=(cfg.batch_size, ))
@@ -41,62 +39,63 @@ class CapsNet(object):
         tf.logging.info('Seting up the main structure')
 
     def build_arch(self):
-        with tf.variable_scope('Conv1_layer'):
-            # Conv1, [batch_size, 20, 20, 256]
-            conv1 = tf.contrib.layers.conv2d(self.X, num_outputs=256,
-                                             kernel_size=9, stride=1,
-                                             padding='VALID')
+        with tf.variable_scope('pi'):
+            with tf.variable_scope('Conv1_layer'):
+                # Conv1, [batch_size, 20, 20, 256]
+                conv1 = tf.contrib.layers.conv2d(self.X, num_outputs=256,
+                                                 kernel_size=9, stride=1,
+                                                 padding='VALID')
 
-        # Primary Capsules layer, return [batch_size, 1152, 8, 1]
-        with tf.variable_scope('PrimaryCaps_layer'):
-            primaryCaps = CapsLayer(num_outputs=8, vec_len=8, with_routing=False, layer_type='CONV')
-            caps1 = primaryCaps(conv1, kernel_size=9, stride=2)
+            # Primary Capsules layer, return [batch_size, 1152, 8, 1]
+            with tf.variable_scope('PrimaryCaps_layer'):
+                primaryCaps = CapsLayer(num_outputs=8, vec_len=8, with_routing=False, layer_type='CONV')
+                caps1 = primaryCaps(conv1, kernel_size=9, stride=2)
 
-        # DigitCaps layer, return [batch_size, 10, 16, 1]
-        with tf.variable_scope('DigitCaps_layer'):
-            digitCaps = CapsLayer(num_outputs=10, vec_len=16, with_routing=True, layer_type='FC')
-            self.caps2 = digitCaps(caps1)
+            # DigitCaps layer, return [batch_size, 10, 16, 1]
+            with tf.variable_scope('DigitCaps_layer'):
+                digitCaps = CapsLayer(num_outputs=10, vec_len=16, with_routing=True, layer_type='FC')
+                self.caps2 = digitCaps(caps1)
 
-        # Decoder structure in Fig. 2
-        # 1. Do masking, how:
-        with tf.variable_scope('Masking'):
-            # a). calc ||v_c||, then do softmax(||v_c||)
-            # [batch_size, 10, 16, 1] => [batch_size, 10, 1, 1]
-            self.v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2),
-                                                  axis=2, keep_dims=True) + epsilon)
-            self.softmax_v = tf.nn.softmax(self.v_length, dim=1)
+            # Decoder structure in Fig. 2
+            # 1. Do masking, how:
+            with tf.variable_scope('Masking'):
+                # a). calc ||v_c||, then do softmax(||v_c||)
+                # [batch_size, 10, 16, 1] => [batch_size, 10, 1, 1]
+                self.v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2),
+                                                      axis=2, keep_dims=True) + epsilon)
+                self.softmax_v = tf.nn.softmax(self.v_length, dim=1)
 
-            # b). pick out the index of max softmax val of the 10 caps
-            # [batch_size, 10, 1, 1] => [batch_size] (index)
-            self.argmax_idx = tf.to_int32(tf.argmax(self.softmax_v, axis=1))
-            self.argmax_idx = tf.reshape(self.argmax_idx, shape=(cfg.batch_size, ))
+                # b). pick out the index of max softmax val of the 10 caps
+                # [batch_size, 10, 1, 1] => [batch_size] (index)
+                self.argmax_idx = tf.to_int32(tf.argmax(self.softmax_v, axis=1))
+                self.argmax_idx = tf.reshape(self.argmax_idx, shape=(cfg.batch_size, ))
 
-            # Method 1.
-            if not cfg.mask_with_y:
-                # c). indexing
-                # It's not easy to understand the indexing process with argmax_idx
-                # as we are 3-dim animal
-                masked_v = []
-                for batch_size in range(cfg.batch_size):
-                    v = self.caps2[batch_size][self.argmax_idx[batch_size], :]
-                    masked_v.append(tf.reshape(v, shape=(1, 1, 16, 1)))
+                # Method 1.
+                if not cfg.mask_with_y:
+                    # c). indexing
+                    # It's not easy to understand the indexing process with argmax_idx
+                    # as we are 3-dim animal
+                    masked_v = []
+                    for batch_size in range(cfg.batch_size):
+                        v = self.caps2[batch_size][self.argmax_idx[batch_size], :]
+                        masked_v.append(tf.reshape(v, shape=(1, 1, 16, 1)))
 
-                self.masked_v = tf.concat(masked_v, axis=0)
-            # Method 2. masking with true label, default mode
-            else:
-                # self.masked_v = tf.matmul(tf.squeeze(self.caps2), tf.reshape(self.Y, (-1, 10, 1)), transpose_a=True)
-                self.masked_v = tf.multiply(tf.squeeze(self.caps2), tf.reshape(self.Y, (-1, 10, 1)))
-                self.v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2), axis=2, keep_dims=True) + epsilon)
+                    self.masked_v = tf.concat(masked_v, axis=0)
+                # Method 2. masking with true label, default mode
+                else:
+                    # self.masked_v = tf.matmul(tf.squeeze(self.caps2), tf.reshape(self.Y, (-1, 10, 1)), transpose_a=True)
+                    self.masked_v = tf.multiply(tf.squeeze(self.caps2), tf.reshape(self.Y, (-1, 10, 1)))
+                    self.v_length = tf.sqrt(tf.reduce_sum(tf.square(self.caps2), axis=2, keep_dims=True) + epsilon)
 
-        # 2. Reconstructe the MNIST images with 3 FC layers
-        # [batch_size, 1, 16, 1] => [batch_size, 16] => [batch_size, 512]
-        with tf.variable_scope('Decoder'):
-            vector_j = tf.reshape(self.masked_v, shape=(cfg.batch_size, -1))
-            fc1 = tf.contrib.layers.fully_connected(vector_j, num_outputs=256)
+            # 2. Reconstructe the MNIST images with 3 FC layers
+            # [batch_size, 1, 16, 1] => [batch_size, 16] => [batch_size, 512]
+            with tf.variable_scope('Decoder'):
+                vector_j = tf.reshape(self.masked_v, shape=(cfg.batch_size, -1))
+                fc1 = tf.contrib.layers.fully_connected(vector_j, num_outputs=256)
 
-            fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=512)
+                fc2 = tf.contrib.layers.fully_connected(fc1, num_outputs=512)
 
-            self.decoded = tf.contrib.layers.fully_connected(fc2, num_outputs=784, activation_fn=tf.sigmoid)
+                self.decoded = tf.contrib.layers.fully_connected(fc2, num_outputs=784, activation_fn=tf.sigmoid)
 
     def loss(self):
         # 1. The margin loss
